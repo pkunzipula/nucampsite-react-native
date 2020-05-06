36a81380f7b61ba7ce8d54a0130975ad039f40dc
Kane Toomer <kanetoomer@yahoo.com>
2020-05-05 19:59:58 -0700

    Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
 import { postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state => {
@@ -23,9 +24,11 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
 
     const {campsite} = props;
+
     if (campsite) {
         return (
-            <Card
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
                 <Text style={{margin: 10}}>
@@ -52,6 +55,7 @@ function RenderCampsite(props) {
                     />
                 </View>
             </Card>
+            </Animatable.View>
         );
     }
     return <View />;
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -11,16 +12,18 @@ class Contact extends Component {
     render() {
         return(
             <ScrollView>
-                <Card wrapperStyle={{margin: 20}} title = 'Contact information'>
-                    <Text style={{marginBottom: 10}}>
-                        1 Nucamp Way {"\n"}
-                        Seattle, WA 98001 {"\n"}
-                        U.S.A. {"\n"}
-                        {"\n"}
-                        Phone: 1-206-555-1234 {"\n"}
-                        Email: campsites@nucamp.co {"\n"}
-                    </Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card wrapperStyle={{margin: 20}} title = 'Contact information'>
+                        <Text style={{marginBottom: 10}}>
+                            1 Nucamp Way {"\n"}
+                            Seattle, WA 98001 {"\n"}
+                            U.S.A. {"\n"}
+                            {"\n"}
+                            Phone: 1-206-555-1234 {"\n"}
+                            Email: campsites@nucamp.co {"\n"}
+                        </Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
