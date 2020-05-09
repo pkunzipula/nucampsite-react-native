d211b5772cec5c5fc66489440940693393da78ce
pkunzipula <jcapra76@gmail.com>
2020-05-08 23:08:47 -0700

    Gestures Part I
    Using React Native's built-in PanResponder... worksss. kinda...

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import * as Animatable from 'react-native-animatable';
+import React, { Component } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 class Contact extends Component {
+  static navigationOptions = {
+    title: "Contact Us",
+  };
 
-    static navigationOptions = {
-        title: 'Contact Us'
-    }
-
-    render() {
-        return(
-            <ScrollView>
-                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
-                    <Card wrapperStyle={{margin: 20}} title = 'Contact information'>
-                        <Text style={{marginBottom: 10}}>
-                            1 Nucamp Way {"\n"}
-                            Seattle, WA 98001 {"\n"}
-                            U.S.A. {"\n"}
-                            {"\n"}
-                            Phone: 1-206-555-1234 {"\n"}
-                            Email: campsites@nucamp.co {"\n"}
-                        </Text>
-                    </Card>
-                </Animatable.View>
-            </ScrollView>
-        );
-    }
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={200} delay={1000}>
+          <Card wrapperStyle={{ margin: 20 }} title="Contact information">
+            <Text style={{ marginBottom: 10 }}>
+              1 Nucamp Way {"\n"}
+              Seattle, WA 98001 {"\n"}
+              U.S.A. {"\n"}
+              {"\n"}
+              Phone: 1-206-555-1234 {"\n"}
+              Email: campsites@nucamp.co {"\n"}
+            </Text>
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
